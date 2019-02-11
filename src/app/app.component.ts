91274940081e00fc2365b9b18a95d901460531fe
pierluigi.summo <s.pierluigi91@hotmail.com>
2019-02-11 12:39:24 +0100

    Loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {TodosService} from './todos.service';
 import {Todo} from './model/todo';
+import {finalize} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,10 @@ import {Todo} from './model/todo';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   todos: Todo[] = [];
   selectedTodo: Todo;
+  loading = true;
 
   constructor(private todosService: TodosService) {
     const successHandler = (response) => {
@@ -19,11 +22,17 @@ export class AppComponent {
       console.log(error);
     }
     this.todosService.getTodos()
-      .pipe()
+      .pipe(
+        finalize(() => this.loading = false)
+      )
       .subscribe(successHandler, errorHandler);
   }
   todoWasSelected(todo: Todo) {
+    this.loading = true;
     this.todosService.getTodoById(todo.id)
+      .pipe(
+        finalize(() => this.loading = false)
+      )
       .subscribe(todoDetail => {
         this.selectedTodo = todoDetail;
       });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { TodoDetailComponent } from './todo-detail/todo-detail.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoDetailComponent
+    TodoDetailComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
